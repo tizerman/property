a6f28fe0310b511608c429c68fb53c51df2fa05d
Yevhenii Tokmakov <matestudent0@gmail.com>
2024-12-27 22:20:31 +0200

    bug fix

--- utils/requests.js
+++ utils/requests.js
@@ -1,16 +1,20 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
 // Fetch all properties
-async function fetchProperties({ showFeatured = false } = {} ) {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
+    // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return []
     }
 
-    const res = await fetch(`${apiDomain}/properties${showFeatured ? '/featured' : ''}`, { cache: 'no-store' })
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      { cache: 'no-store' }
+    )
 
     if (!res.ok) {
-      throw new Error('Failed to fetch data!')
+      throw new Error('Failed to fetch data')
     }
 
     return res.json()
@@ -20,17 +24,18 @@ async function fetchProperties({ showFeatured = false } = {} ) {
   }
 }
 
-//Fetch single property
+// Fetch single property
 async function fetchProperty(id) {
   try {
+    // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return null
     }
 
-    const res = await fetch(`${apiDomain}/properties/${id}`, { cache: 'no-store' })
+    const res = await fetch(`${apiDomain}/properties/${id}`)
 
     if (!res.ok) {
-      throw new Error('Failed to fetch data!')
+      throw new Error('Failed to fetch data')
     }
 
     return res.json()
