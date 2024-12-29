9b8e7108853defdc0a60a4346aed56e7f25bf608
Yevhenii Tokmakov <matestudent0@gmail.com>
2024-12-29 16:04:48 +0200

    fix1

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     <>
       <Hero />
       <InfoBoxes />
-      <FeaturedProperties />
+      {/* <FeaturedProperties /> */}
       <HomeProperties />
     </>
   )
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -3,15 +3,15 @@ import PropertyCard from '@/components/PropertyCard'
 import { fetchProperties } from '@/utils/requests'
 
 const HomeProperties = async () => {
-  const data = await fetchProperties()
+  // const data = await fetchProperties()
 
-  const recentProperties = data.properties
-    .sort(() => Math.random() - Math.random())
-    .slice(0, 3)
+  // const recentProperties = data.properties
+  //   .sort(() => Math.random() - Math.random())
+  //   .slice(0, 3)
 
   return (
     <>
-      <section className='px-4 py-6'>
+      {/* <section className='px-4 py-6'>
         <div className='container-xl lg:container m-auto'>
           <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
             Recent Properties
@@ -26,7 +26,7 @@ const HomeProperties = async () => {
             )}
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section className='m-auto max-w-lg my-10 px-6'>
         <Link
