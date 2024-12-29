23696e2193ac29e2f69c95c98169aad4aea15f6b
Yevhenii Tokmakov <matestudent0@gmail.com>
2024-12-29 13:23:16 +0200

    prod

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -48,9 +48,7 @@ const Properties = () => {
           <p>No properties found</p>
         ) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {properties
-              // .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-              .map((property) => (
+            {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
           </div>
