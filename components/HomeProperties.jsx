bb5274bd29162de9a590d0fe3d5a479bf2235709
Yevhenii Tokmakov <matestudent0@gmail.com>
2024-12-29 16:42:45 +0200

    fix3

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -4,10 +4,11 @@ import { fetchProperties } from '@/utils/requests'
 
 const HomeProperties = async () => {
   const data = await fetchProperties()
+  console.log(data)
 
-  const recentProperties = await data.properties
-    .sort(() => Math.random() - Math.random())
-    .slice(0, 3)
+  // const recentProperties = data.properties
+  //   .sort(() => Math.random() - Math.random())
+  //   .slice(0, 3)
 
   return (
     <>
